e89ebe179be8a8fd4bfd589046d5b9b55ba09f99
TomasSandoval-tms <tomasando32@gmail.com>
2026-01-08 19:32:12 -0300

    Update plantilla.js

--- plantilla.js
+++ plantilla.js
@@ -1,6 +1,6 @@
 
 function obtenerEmojisAleatorios(cantidad) {
-  const emojisDisponibles = ['🎫', '🐸', '🔮', '💚', '💊', '🩷', '💰', '💛', '💍', '🩵', '🧡', '💡', '💌'];
+  const emojisDisponibles = ['🎫', '🐸', '❤️', '⏱️', '🪞', '🖤', '🪄', '🔮', '💚', '💊', '🩷', '💰', '💛', '💍', '🩵', '🧡', '💡', '💌'];
   const obligatorios = ['🎫', '🐸'];
   const copia = emojisDisponibles.filter(e => !obligatorios.includes(e));
   const seleccionados = [...obligatorios];
@@ -99,4 +99,5 @@ function iniciarJuego() {
 
   document.getElementById('btn-carton').addEventListener('click', iniciarCarton);
 }
+
 
